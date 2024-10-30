2da391323c1203a238a39f31a2d41bbd45b55f05
Hieu <danghieuyt2@gmail.com>
2024-10-30 15:09:23 +0700

    update: role shop guard

--- src/auth/roles/role.shop.guard.ts
+++ src/auth/roles/role.shop.guard.ts
@@ -1,14 +1,16 @@
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException, BadRequestException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleShop } from './role.shop.enum';
 import { ROLES_SHOP_KEY } from './role.shop.decorator';
 import { PrismaService } from '../../database/prisma.service';
+import { ShopRepository } from 'src/shop/repositories/shop.repository';
 
 @Injectable()
 export class RolesShopGuard implements CanActivate {
     constructor(
         private reflector: Reflector,
-        private prisma: PrismaService
+        private prisma: PrismaService,
+        private shopRepository: ShopRepository
     ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -24,6 +26,10 @@ export class RolesShopGuard implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const { user } = request;
         const shopId = Number(request.params.shopId);
+
+        // check shop existed?
+        const foundShop = await this.shopRepository.findShopById(shopId)
+        if (!foundShop) throw new BadRequestException('Shop not found or no longger exist')
         
         // check role in shop
         const shopUser = await this.prisma.shopUser.findFirst({
